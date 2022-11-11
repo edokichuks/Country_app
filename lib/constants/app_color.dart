5a449f737a79655421dec41724fe9e37cfb80b63
Edoki Chukwuyem <edokichuks@gmail.com>
2022-11-11 19:41:31 +0100

    folder structure ready

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static Color mainColor = Colors.blueGrey.shade900;
+  static Color textColor = Colors.blueGrey.shade400;
+  
+}
---
+++ lib/constants/app_string.dart
@@ -0,0 +1,6 @@
+class AppString {
+  ///This class holds all the string the app and its a private class hence its member are static.
+
+  AppString._();
+  static const String explore = 'Explore';
+}
