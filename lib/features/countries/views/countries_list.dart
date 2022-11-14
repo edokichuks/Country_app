30e5f98834d15434f9ccb944b21ac9611a4e5226
Edoki Chukwuyem <edokichuks@gmail.com>
2022-11-14 07:36:13 +0100

    search functionality added

--- lib/features/countries/views/countries_list.dart
+++ lib/features/countries/views/countries_list.dart
@@ -18,6 +18,48 @@ class Countries extends ConsumerStatefulWidget {
 }
 
 class _CountriesState extends ConsumerState<Countries> {
+  late final TextEditingController searchController;
+  List<Country> searchedCountry = [];
+  List<Country> countryInternalList = [];
+
+  bool isSearching = false;
+
+  @override
+  void initState() {
+    searchController = TextEditingController();
+    searchController.addListener(searchCountryListener);
+    super.initState();
+  }
+
+  void searchCountryListener() {
+    if (searchController.text.trim().isNotEmpty) {
+      String searchQuery = searchController.text.trim().toLowerCase();
+      List<Country> tempList = [];
+
+      for (int i = 0; i < countryInternalList.length; i++) {
+        final singleCountry = countryInternalList[i];
+
+        if (singleCountry.name!.common!.toLowerCase().contains(searchQuery)) {
+          tempList.add(singleCountry);
+        }
+      }
+      setState(() {
+        searchedCountry = tempList;
+        isSearching = true;
+      });
+    } else {
+      setState(() {
+        isSearching = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,6 +96,7 @@ class _CountriesState extends ConsumerState<Countries> {
             height: 5,
           ),
           TextFormField(
+            controller: searchController,
             textAlign: TextAlign.center,
             decoration: InputDecoration(
               hintText: AppString.searchCountry,
@@ -74,17 +117,96 @@ class _CountriesState extends ConsumerState<Countries> {
                   ref.watch(countriesListProvider);
               return countryList.when(
                   data: (data) {
-                    return ListView.builder(
+                    countryInternalList = data;
+
+
+                    return 
+                    isSearching && searchedCountry.isEmpty?
+                    
+                    Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const SizedBox(
+                                  height: 100,
+                                ),
+                                Icon(
+                                  Icons.search_off,
+                                  size: 100,
+                                  color: AppColor.mainColor,
+                                ),
+                                Text(
+                                  'No results found',
+                                  style: TextStyle(
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColor.mainColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        :
+                        isSearching ?
+                        ListView.builder(
+                        itemCount: searchedCountry.length,
+                        physics: const BouncingScrollPhysics(),
+                        
+                        itemBuilder: (context, index) {
+                          final singleCountry = searchedCountry[index];
+                          return InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) {
+                                    return CountryDetails(
+                                        country: singleCountry);
+                                  },
+                                ),
+                              );
+                            },
+                            child: ListTile(
+                              leading: Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: AppColor.transperent,
+                                  image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                      singleCountry.flags!.png ??
+                                          AppString.flag,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              title: Text(singleCountry.name!.common ??
+                                  AppString.country),
+                              subtitle: Text(singleCountry.capital != null
+                                  ? singleCountry.capital!.join('')
+                                  : singleCountry.capital.toString()),
+                            ),
+                          );
+                        }):
+
+                    ListView.builder(
                         itemCount: data.length,
                         physics: const BouncingScrollPhysics(),
                         itemBuilder: (context, index) {
                           final singleCountry = data[index];
                           return InkWell(
                             onTap: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (_) {
-                                return CountryDetails(country: singleCountry);
-                              }));
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) {
+                                    return CountryDetails(
+                                        country: singleCountry);
+                                  },
+                                ),
+                              );
                             },
                             child: ListTile(
                               leading: Container(
